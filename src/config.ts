5db4957c58c1a8adc695e1d45ab0541920a597f4
P.Siri Reddy <saisirireddy27@gmail.com>
2026-06-25 20:42:20 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+eexport const SITE_TITLE = 'Sai Siri Reddy | Portfolio';
+export const SITE_DESCRIPTION = 'Personal engineering portfolio showcasing full-stack web applications, technical projects, and development experience.';
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
